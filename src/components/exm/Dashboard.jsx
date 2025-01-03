44b842091c6872df62299de75d78cd05cc631258
tash-working <tashrifatwork@gmail.com>
2025-01-03 19:18:28 +0600

    ok

--- src/components/exm/Dashboard.jsx
+++ src/components/exm/Dashboard.jsx
@@ -24,6 +24,34 @@ const Dashboard = () => {
       }
       const posOrders = await response.json();
       setOrders(posOrders);
+
+      function generateSalesReportByDate(data) {
+        const salesByDate = {};
+      
+        data.forEach((order) => {
+          const orderDate = new Date(order.date_time).toLocaleDateString();
+      
+          if (!salesByDate[orderDate]) {
+            salesByDate[orderDate] = {
+              totalSales: 0,
+              orders: [],
+            };
+          }
+      
+          salesByDate[orderDate].totalSales += order.price;
+          salesByDate[orderDate].orders.push(order);
+        });
+      
+        const salesReportArray = Object.entries(salesByDate).map(([date, salesData]) => ({
+          date,
+          ...salesData,
+        }));
+        console.log(salesReportArray);
+        
+         
+      }
+      generateSalesReportByDate(posOrders)
+
       localStorage.setItem("orderHistory", JSON.stringify(posOrders));
 
       const billsByType = posOrders.reduce((acc, item) => {
@@ -96,78 +124,112 @@ const Dashboard = () => {
 
 
       <div className="my-8">
-
-        <h2 className="text-2xl font-bold text-center mb-4">Payment Breakdown Chart</h2>
-        <div className="my-4 text-center">
-          <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-gray-800 via-black to-gray-900 text-4xl">
-            Total Amount: {orders.reduce((sum, item) => sum + item.price, 0)} TK
-          </h1>
-        </div>
-        <div className="my-4 flex flex-wrap gap-4 justify-center">
-          <div
-            id="cash"
-            className="px-6 py-3 bg-[#36454F] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
-          >
-           
-            <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
-            <FaMoneyBill size={20} />Cash
-        </div>
-            <hr />
-            <p>Total Orders : {cash.length}</p>
-            <p>Amount : {cash.reduce((sum, item) => sum + item.price, 0)} TK</p>
-          </div>
-          <div
-            id="mobileBanking"
-            className="px-6 py-3 bg-[#023020] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
-          >
-           <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
-            <FaMobileAlt/>
-           <FaMoneyCheck />
-            Mobile Banking
-        </div>
-            <hr />
-            <p>Total Orders : {mb.length}</p>
-            <p>Amount : {mb.reduce((sum, item) => sum + item.price, 0)} TK</p>
-          </div>
-          <div
-            id="card"
-            className="px-6 py-3 bg-[#301934] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
-          >
-             
-             <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
-             <FaMoneyCheck />Card
-        </div>
-            <hr />
-            <p>Total Orders : {card.length}</p>
-            <p>Amount : {card.reduce((sum, item) => sum + item.price, 0)} TK</p>
-          </div>
-          <div
-            id="due"
-            className="px-6 py-3 bg-[#343434] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
-          >
-            
-            <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
-            <FaExclamationCircle />Due
-        </div>
-            <hr />
-            <p>Total Orders : {due.length}</p>
-            <p>Amount : {due.reduce((sum, item) => sum + item.price, 0)} TK</p>
-          </div>
-
-          <Link to="/dashboard/history">
-            <button style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}
-              id="btn"
-              className="px-6 py-3 bg-[#343434] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
-            >
-              See Order history <FaArrowRight />
-            </button>
-          </Link>
-        </div>
-
-        <div className="w-full max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
-          <Doughnut data={chartData} options={chartOptions} />
-        </div>
+  <h2 className="text-2xl font-bold text-center mb-4">Payment Breakdown Chart</h2>
+  <div className="my-4 text-center">
+    <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-gray-800 via-black to-gray-900 text-4xl">
+      Total Amount: {orders.reduce((sum, item) => sum + item.price, 0)} TK
+    </h1>
+  </div>
+  <div className="my-4 flex flex-wrap gap-4 justify-center">
+    <div
+      id="cash"
+      className="px-6 py-3 bg-[#36454F] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
+    >
+      <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
+        <FaMoneyBill size={20} /> Cash
       </div>
+      <hr />
+      <p>Total Orders: {cash.length}</p>
+      <p>Amount: {cash.reduce((sum, item) => sum + item.price, 0)} TK</p>
+      <p>
+        Percentage:{" "}
+        {(
+          (cash.reduce((sum, item) => sum + item.price, 0) /
+            orders.reduce((sum, item) => sum + item.price, 0)) *
+          100
+        ).toFixed(2)}
+        %
+      </p>
+    </div>
+    <div
+      id="mobileBanking"
+      className="px-6 py-3 bg-[#023020] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
+    >
+      <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
+        <FaMobileAlt />
+        <FaMoneyCheck />
+        Mobile Banking
+      </div>
+      <hr />
+      <p>Total Orders: {mb.length}</p>
+      <p>Amount: {mb.reduce((sum, item) => sum + item.price, 0)} TK</p>
+      <p>
+        Percentage:{" "}
+        {(
+          (mb.reduce((sum, item) => sum + item.price, 0) /
+            orders.reduce((sum, item) => sum + item.price, 0)) *
+          100
+        ).toFixed(2)}
+        %
+      </p>
+    </div>
+    <div
+      id="card"
+      className="px-6 py-3 bg-[#301934] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
+    >
+      <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
+        <FaMoneyCheck /> Card
+      </div>
+      <hr />
+      <p>Total Orders: {card.length}</p>
+      <p>Amount: {card.reduce((sum, item) => sum + item.price, 0)} TK</p>
+      <p>
+        Percentage:{" "}
+        {(
+          (card.reduce((sum, item) => sum + item.price, 0) /
+            orders.reduce((sum, item) => sum + item.price, 0)) *
+          100
+        ).toFixed(2)}
+        %
+      </p>
+    </div>
+    <div
+      id="due"
+      className="px-6 py-3 bg-[#343434] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
+    >
+      <div style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}>
+        <FaExclamationCircle /> Due
+      </div>
+      <hr />
+      <p>Total Orders: {due.length}</p>
+      <p>Amount: {due.reduce((sum, item) => sum + item.price, 0)} TK</p>
+      <p>
+        Percentage:{" "}
+        {(
+          (due.reduce((sum, item) => sum + item.price, 0) /
+            orders.reduce((sum, item) => sum + item.price, 0)) *
+          100
+        ).toFixed(2)}
+        %
+      </p>
+    </div>
+    <Link to="/dashboard/history">
+      <button
+        style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}
+        id="btn"
+        className="px-6 py-3 bg-[#343434] text-white text-center cursor-pointer rounded hover:bg-[#717786]"
+      >
+        See Order History <FaArrowRight />
+      </button>
+    </Link>
+  </div>
+  <div className="w-full max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
+    <Doughnut data={chartData} options={chartOptions} />
+  </div>
+</div>
+
+
+
     </div>
   );
 };
